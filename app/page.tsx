a4276b4118974a11f52368b9cfa27ca34880806c
patrick <trickmeredor@gmail.com>
2026-01-04 01:27:27 +0800

    Added components at page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-import Navbar from "./navbar/page";
-import Hero from "./hero/page";
+import Navbar from "./(pages)/navbar/page";
+import Hero from "./(pages)/hero/page";
+import About from "./(pages)/about/page";
+import Projects from "./(pages)/projects/page";
+import Footer from "./(pages)/footer/page";
+
 export default function Home() {
   return (
-    <div>
+    <div className="App flex flex-col items-center">
       <Navbar />
-      <Hero /> 
+      <Hero />
+      <About />
+      <Projects />
+      <Footer />
     </div>
   );
 }
