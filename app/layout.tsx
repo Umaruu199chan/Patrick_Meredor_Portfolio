10ec4af923c5bd44241c25ff47c2f8369567a633
patrick <trickmeredor@gmail.com>
2026-01-04 01:28:10 +0800

    Imported Google Fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/ui/shared/theme-provider";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Bebas_Neue, Inter, Gasoek_One } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -13,6 +18,18 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const bebasNeue = Bebas_Neue({
+  weight: ["400"],
+  subsets: ["latin"],
+  style: ["normal"],
+  variable: "--font-bebas-neue",
+});
+
+const gasoekOne = Gasoek_One({
+  weight: ["400"],
+  variable: "--font-gasoek-one",
+  subsets: ["latin"],
+})
 export const metadata: Metadata = {
   title: "Patrick Meredor",
   description: "Portfolio of Patrick Meredor",
@@ -28,8 +45,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${bebasNeue.variable} ${inter.variable} ${gasoekOne.variable} antialiased`}
       >
         <ThemeProvider
             attribute="class"
