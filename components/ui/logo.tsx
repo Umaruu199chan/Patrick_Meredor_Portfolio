4168b7c1010f9a614c9be015a3eb888d74ea4e16
patrick <trickmeredor@gmail.com>
2026-01-04 01:26:42 +0800

    Added logo and make it clickable

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,7 +1,16 @@
+"use client";
 import Image from "next/image";
-export default function Logo() {    
+
+interface LogoProps {
+    onClick?: () => void;
+}
+
+export default function Logo({ onClick }: LogoProps) {    
     return (
-        <div>
+        <div 
+            onClick={onClick}
+            className={onClick ? "cursor-pointer transition-opacity hover:opacity-80" : ""}
+        >
             <Image src="/Logo.svg" alt="logo" width={50} height={50} className="dark:invert"/>
         </div>
     )
