8da3760699f5efbb875af5c4c86b750760326ad6
patrick <trickmeredor@gmail.com>
2026-01-04 01:27:58 +0800

    Remove this contents

--- app/hero/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Hero() {
-    return (
-        <div className="min-h-100vh">
-            <div>
-                <h1>PATRICK MEREDOR</h1>
-            </div>
-        </div>
-    )
-}
