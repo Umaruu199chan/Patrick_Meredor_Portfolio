280a10a9f9be543f4518d0924825e81a47593541
patrick <trickmeredor@gmail.com>
2026-01-04 01:27:06 +0800

    Added button component
    i also make it responsive

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -21,12 +21,12 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-        icon: "size-9",
-        "icon-sm": "size-8",
-        "icon-lg": "size-10",
+        default: "h-9 px-4 py-2 text-sm sm:h-9 sm:px-4 sm:text-base has-[>svg]:px-2.5 sm:has-[>svg]:px-3",
+        sm: "h-7 rounded-md gap-1.5 px-2.5 text-xs sm:h-8 sm:px-3 sm:text-sm has-[>svg]:px-2 sm:has-[>svg]:px-2.5",
+        lg: "h-9 px-5 text-base sm:h-10 sm:px-6 sm:text-lg rounded-md has-[>svg]:px-3 sm:has-[>svg]:px-4",
+        icon: "size-8 sm:size-9",
+        "icon-sm": "size-7 sm:size-8",
+        "icon-lg": "size-9 sm:size-10",
       },
     },
     defaultVariants: {
